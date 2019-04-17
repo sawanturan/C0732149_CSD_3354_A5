5ba0bc836c98b6fcdb5413d5072317357008befd
visualstudio <visualstudio@122-PC15>
2019-04-17 09:22:10 -0400

    Commit

--- C0732149_CSD_3354_A5/Program.cs
+++ C0732149_CSD_3354_A5/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace C0732149_CSD_3354_A5
 {
@@ -12,16 +13,21 @@ namespace C0732149_CSD_3354_A5
         {
             { new GenerateList().Run(57); }
         }
-        class GenerateList
-        {
-            const int LEN = 100;
-            ArrayList ListQ;
-            public void Run(intAddUptoNumber)
+    class GenerateList
+    {
+        const int LEN = 100;
+        ArrayList ListQ;
+        public void Run(int AddUpToNumber)
+        {Random r = new Random();
+
+         ListQ = new ArrayList();
+          for (int i = 0; i < LEN; i++) { ListQ.Add(r.Next(100)); }
+          this.findTwoNumbersThatAddUpto(AddUpToNumber);
+        }
+
+            private void findTwoNumbersThatAddUpto(int addUpToNumber)
             {
-                Random r = new Random();
-                ListQ = new ArrayList();
-                for (int i = 0; i < LEN; i++) { ListQ.Add(r.Next(100)); }
-                this.findTwoNumbersThatAddUpto(AddUpToNumber);
+                throw new NotImplementedException();
             }
         }
     }
