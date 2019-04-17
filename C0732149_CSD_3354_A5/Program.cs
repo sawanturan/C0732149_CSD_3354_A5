a37ebd2ba9e343dbba6bb2fc94350384fb4d4bc5
visualstudio <visualstudio@122-PC15>
2019-04-17 09:40:15 -0400

    SummingNumbers

--- C0732149_CSD_3354_A5/Program.cs
+++ C0732149_CSD_3354_A5/Program.cs
@@ -11,24 +11,45 @@ namespace C0732149_CSD_3354_A5
     {
         static void Main(string[] args)
         {
-            { new GenerateList().Run(57); }
+            GenerateList a = new GenerateList();
+            a.Run(57); 
         }
-    class GenerateList
-    {
-        const int LEN = 100;
-        ArrayList ListQ;
-        public void Run(int AddUpToNumber)
-        {Random r = new Random();
+        class GenerateList
+        {
+            const int LEN = 100;
 
-         ListQ = new ArrayList();
-          for (int i = 0; i < LEN; i++) { ListQ.Add(r.Next(100)); }
-          this.findTwoNumbersThatAddUpto(AddUpToNumber);
-        }
-
-            private void findTwoNumbersThatAddUpto(int addUpToNumber)
+            ArrayList ListQ;
+            public void Run(int AddUpToNumber)
             {
-                throw new NotImplementedException();
+                Random r = new Random();
+
+                ListQ = new ArrayList();
+                for (int i = 0; i < LEN; i++)
+                {
+                    ListQ.Add(r.Next(100));
+                    this.findTwoNumbersThatAddUpto(AddUpToNumber);
+                    break;
+                }
+            }
+
+            public void findTwoNumbersThatAddUpto(int a)
+            {
+                Random r = new Random();
+                int n1 = 0;
+                int n2 = 0;
+                for (int i = 0; i < LEN; i++)
+                {
+                    int num1 = ListQ.Add(r.Next(100));
+                    int num2 = ListQ.Add(r.Next(100));
+                    if ( num1 + num2 == a)
+                    {
+                        n1 = num1;
+                        n2 = num2;
+                        Console.WriteLine($"The Two Numbers are the Folowing {n1} and {n2}.");
+                    }
+                }
             }
         }
     }
 }
+
