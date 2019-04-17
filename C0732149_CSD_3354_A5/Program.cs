7ceacc38002f53134701bb6d0dc25eee55196618
visualstudio <visualstudio@122-PC15>
2019-04-17 08:46:15 -0400

    Commit

--- C0732149_CSD_3354_A5/Program.cs
+++ C0732149_CSD_3354_A5/Program.cs
@@ -10,6 +10,19 @@ namespace C0732149_CSD_3354_A5
     {
         static void Main(string[] args)
         {
+            { new GenerateList().Run(57); }
+        }
+        class GenerateList
+        {
+            const int LEN = 100;
+            ArrayList ListQ;
+            public void Run(intAddUptoNumber)
+            {
+                Random r = new Random();
+                ListQ = new ArrayList();
+                for (int i = 0; i < LEN; i++) { ListQ.Add(r.Next(100)); }
+                this.findTwoNumbersThatAddUpto(AddUpToNumber);
+            }
         }
     }
 }
